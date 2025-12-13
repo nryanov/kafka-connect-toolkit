41a016f8443555e292933b4e5425fce1e02e08f2
Nikita Ryanov <ryanov.nikita@gmail.com>
2025-12-13 05:00:02 +0300

    feat: add MaskFieldValue transform

--- modules/toolkit/src/main/java/com/nryanov/kafka/connect/toolkit/transforms/MaskFieldValue.java
+++ modules/toolkit/src/main/java/com/nryanov/kafka/connect/toolkit/transforms/MaskFieldValue.java
@@ -1,4 +1,29 @@
 package com.nryanov.kafka.connect.toolkit.transforms;
 
-public class MaskFieldValue {
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.transforms.Transformation;
+
+import java.util.Map;
+
+public class MaskFieldValue<R extends ConnectRecord<R>> implements Transformation<R> {
+    @Override
+    public ConfigDef config() {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+
+    }
+
+    @Override
+    public R apply(R record) {
+        return null;
+    }
 }
