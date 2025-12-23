558e8f890ffe650fa6e9f162f98bf966f7bf236c
Nikita Ryanov <ryanov.nikita@gmail.com>
2025-12-24 00:54:12 +0300

    update

--- modules/toolkit/src/main/java/com/nryanov/kafka/connect/toolkit/transforms/ConcatFields.java
+++ modules/toolkit/src/main/java/com/nryanov/kafka/connect/toolkit/transforms/ConcatFields.java
@@ -1,5 +1,8 @@
 package com.nryanov.kafka.connect.toolkit.transforms;
 
+import com.nryanov.kafka.connect.toolkit.transforms.common.ConfigParser;
+import com.nryanov.kafka.connect.toolkit.transforms.common.FieldFilter;
+import org.apache.kafka.common.config.AbstractConfig;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.ConnectRecord;
 import org.apache.kafka.connect.transforms.Transformation;
@@ -7,9 +10,32 @@ import org.apache.kafka.connect.transforms.Transformation;
 import java.util.Map;
 
 public class ConcatFields<R extends ConnectRecord<R>> implements Transformation<R> {
+    private final static String FIELDS = "fields";
+    private final static String DEFAULT_DELIMITER = "delimiter";
+
+    private final static ConfigDef CONFIG_DEF =
+            new ConfigDef()
+                    .define(
+                            FIELDS,
+                            ConfigDef.Type.STRING,
+                            null,
+                            ConfigDef.Importance.MEDIUM,
+                            "Comma separated list of fields for concatenation"
+                    )
+                    .define(
+                            DEFAULT_DELIMITER,
+                            ConfigDef.Type.STRING,
+                            "_",
+                            ConfigDef.Importance.MEDIUM,
+                            "Default delimiter"
+                    );
+
+    private FieldFilter filter;
+    private String delimiter;
+
     @Override
     public ConfigDef config() {
-        return null;
+        return CONFIG_DEF;
     }
 
     @Override
@@ -19,7 +45,17 @@ public class ConcatFields<R extends ConnectRecord<R>> implements Transformation<
 
     @Override
     public void configure(Map<String, ?> configs) {
+        var config = new AbstractConfig(CONFIG_DEF, configs);
+        var fieldsRaw = config.getString(FIELDS);
 
+        if (fieldsRaw != null && !"*".equals(fieldsRaw)) {
+            filter = new FieldFilter.Subset(ConfigParser.parseCommaSeparatedSingleValues(fieldsRaw));
+        } else if ("*".equals(fieldsRaw)) {
+            filter = new FieldFilter.All();
+        } else {
+            filter = new FieldFilter.None();
+        }
+        delimiter = config.getString(DEFAULT_DELIMITER);
     }
 
     @Override
