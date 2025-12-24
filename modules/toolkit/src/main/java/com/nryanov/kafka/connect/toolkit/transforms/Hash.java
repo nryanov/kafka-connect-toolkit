eba771a7a57dd2b734857753a09ff50eff494590
Nikita Ryanov <ryanov.nikita@gmail.com>
2025-12-24 03:32:35 +0300

    feat: add Hash transform

---
+++ modules/toolkit/src/main/java/com/nryanov/kafka/connect/toolkit/transforms/Hash.java
@@ -0,0 +1,24 @@
+package com.nryanov.kafka.connect.toolkit.transforms;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.transforms.Transformation;
+
+import java.util.Map;
+
+public abstract class Hash<R extends ConnectRecord<R>> implements Transformation<R> {
+    @Override
+    public ConfigDef config() {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+
+    }
+}
